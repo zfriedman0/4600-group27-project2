4974c12a1b2d400779c650390737daeb6d92390d
Malena <reyesamalena@gmail.com>
2020-04-29 18:43:43 -0500

    temp
    attempt 1

--- proj2/project2.cpp
+++ proj2/project2.cpp
@@ -5,12 +5,145 @@
 #include <regex>    //regex library
 #include <map>
 #include <list>
+#include <vector>
+#include <algorithm>
 
 
 using namespace std;
+/* bool is process a sink */
+bool isSink(vector<vector<int>> AdjList, int process, int num_processes, int num_resources ){
+    int i = process;
+    bool flag=true;
+    if(AdjList[i][0] == -1){ //empty
+        flag = false;
+        return flag;
+    }
+        for(int j = 0; j < AdjList[i].size(); j++)
+        {
+
+            if(j == AdjList[i].size() - 1)
+            {
+                /* if it is a process */
+                if(AdjList[i][j] < num_processes ){
+                 //   cout << " -> P" << AdjList[i][j]  << endl;
+                    break;
+                }else if(AdjList[i][j] >= num_resources  ){
+                    flag=false;
+                 //   cout << " -> R" << (AdjList[i][j] - num_processes)  << endl;
+                    break;
+                }
+            }
+            else{
+                if(AdjList[i][j]<num_processes  ){
+                  //  cout << " -> P" << AdjList[i][j];
+                }else if(AdjList[i][j]>=num_resources  ){
+                    flag=false;
+                   //  cout << " -> R" << (AdjList[i][j] -  num_processes);
+                }
+            }
+        }
+    return flag;
+
+}
+/* reduce function */
+void reduce(vector<vector<int>> &AdjList, int process, int num_processes, int num_resources,vector<int> &used_num_resources_list ){
+    
+    for (int i = 0; i < AdjList.size(); i++)
+    {
+        vector<int>::iterator itr = find(AdjList[i].begin(), AdjList[i].end(), process);
+        if(itr != AdjList[i].cend()){
+          //  cout<< "P" <<process+1 << " present at index " << distance(AdjList[i].begin(), itr)<<endl;
+            int delete_index = distance(AdjList[i].begin(), itr);
+            //AdjList[i].erase (AdjList[i].begin()+delete_index);
+            AdjList[i].at(delete_index) = -1;
+            if(i>=num_processes ){
+                used_num_resources_list[i-num_processes]++;
+            }
+        }else{
+           //cout << "P" <<process+1 << " not found"<<endl;
+        }
+    }
+}
+/* bool is graph fully reducable */
+bool isFullyReduce(vector<vector<int>> &AdjList, int process, int num_processes, int num_resources ){
+    
+    bool flag =true;
+
+        for(int p=0; p< num_processes;p++){
+            if(AdjList[p].size() >= 1 && AdjList[p][0] != -1 ){
+               // cout << AdjList[p][0]<<endl;
+                flag =false;
+            }
+
+        }
+    return flag;
+    
+}
+/* check if requested edges can be fufilled */
+bool request_edge_check(vector<vector<int>> &AdjList, int process, int num_processes, int num_resources, vector<int> &used_num_resources_list, vector<int> num_resources_list ){
+    map<pair<int, int>, int> to_delete_map;
+    int flag=false;
+    int deletedcount=0;
+    for(int p=0; p< num_processes;p++){ //looping through all processes
+        deletedcount=0;
+        if( AdjList[p][0] != -1 ){ //if the process is not requesting anything or empty
+            for(int j=0; j<AdjList[p].size(); j++){ //loop through processes requests
+                if(p != AdjList[p][j]){   //except itself
+                        if((used_num_resources_list[AdjList[p][j] - num_processes] > 0) && (used_num_resources_list[AdjList[p][j] - num_processes] <= num_resources_list[AdjList[p][j] - num_processes]  ) ){ //means the resources are available
+                            cout << "adding P"<<p+1<< " to R"<< AdjList[p][j] - num_processes +1 <<endl;
+                            flag=true;
+                            vector<int>::iterator itr = find(AdjList[AdjList[p][j]].begin(), AdjList[AdjList[p][j]].end(), -1); //finding a negative -1
+                            if(itr != AdjList[AdjList[p][j]].cend()  ){//means a previous has left and resource is available
+                              
+                                
+                                int checkindex =distance(AdjList[AdjList[p][j]].begin(), itr);
+                                if (AdjList[AdjList[p][j]][checkindex+1] == AdjList[p][j] ){
+                                    AdjList[AdjList[p][j]].at(checkindex) = p;
+                                    used_num_resources_list[AdjList[p][j] - num_processes]--;
+                                    to_delete_map[ { p,AdjList[p][j] }]=0;
+                                    to_delete_map[ { p,p }]=-1;
+                                    deletedcount++;
+                                }
+                                       
+                            }else{
+                                AdjList[AdjList[p][j]].push_back(p);
+                                    used_num_resources_list[AdjList[p][j] - num_processes]--;
+                                to_delete_map[ { p, AdjList[p][j] }]=0;
+                                to_delete_map[ { p,p }]=-1;
+                            }
+                        }else if(used_num_resources_list[AdjList[p][j] - num_processes] ==0){ //cannot allocate
+                            //try the next resource request
+                            continue;
+                        }
+                }
+            }
+        }
+                    cout<< endl;
+    }
+    
+    /* delete excess of fufilled */
+
+    map<pair<int, int>, int>::iterator it =to_delete_map.begin();
+    while(  it != to_delete_map.end()){
+        int row = it->first.first;
+        int searchfor =it->first.second;
+        vector<int>::iterator it_r = find(AdjList[row].begin(), AdjList[row].end(), searchfor);
+        if(it_r != AdjList[row].cend()){
+          //  cout << "perform delete" <<endl;
+            int index = distance(AdjList[row].begin(), it_r);
+            if(it->second ==0){
+                
+                AdjList[row].erase (AdjList[row].begin()+ index);
+            }
+        }
+        it++;
+    }
+    return flag;
+}
 // converts from adjacency matrix to adjacency list
-vector<vector<int>> convert( vector<vector<int>> a)
+vector<vector<int>> convert( vector<vector<int>> a, int num_processes, int num_resources, vector<int> &used_num_resources_list )
 {
+    
     vector<vector<int>> adjList(a.size());
     for (int i = 0; i < a.size(); i++)
     {
@@ -19,14 +152,17 @@ vector<vector<int>> convert( vector<vector<int>> a)
         {
             if (a[i][j] == 1)
             {
-                adjList[i].push_back(j+1);
+                adjList[i].push_back(j);
+                if(i>=num_processes && j<num_processes){
+                    used_num_resources_list[i-num_processes]--;
+                }
             }
         }
     }
     return adjList;
 }
 
-void readfile(string textfile,  vector<int> &num_resources_list, vector<vector<int>> &AdjList,     int &num_processes,int &num_resources){
+void readfile(string textfile,  vector<int> &num_resources_list, vector<vector<int>> &AdjList, int &num_processes,int &num_resources,         vector<int> &used_num_resources_list){
     vector<int> result;
     vector<vector<int>> a;
     ifstream configf;
@@ -44,7 +180,9 @@ void readfile(string textfile,  vector<int> &num_resources_list, vector<vector<i
     
     string line;
     smatch equal;
-    regex e ("=");
+        smatch equalr;
+    regex e ("num_processes=");
+    regex er ("num_resources=");
     regex comment ("%[^\n]*"  );
         regex blank ("[ \t\n]"  );
     int check_num_units=0;
@@ -62,22 +200,23 @@ void readfile(string textfile,  vector<int> &num_resources_list, vector<vector<i
                 continue;
             }else{
         
-                //cout<<line<< endl;
+                cout<<line<< endl;
                 regex_search(line, equal, e);
-             
-                if(equal.prefix().str() == "num_processes" ){
+                regex_search(line, equalr, er);
+                if(equal.size() >0){
                     /* converting the suffix to an int and saving num_processes */
                     temp_num_processes = equal.suffix().str();
                     stringstream num_process_convert(temp_num_processes);
                     num_process_convert >> num_processes;
                     check_num_units++;
-                    //cout << "\nthe num_processes is:" << num_processes <<endl;
-                }else if(equal.prefix().str() == "num_resources"){
+                    cout << "\nthe num_processes is:" << num_processes <<endl;
+                    
+                }else if(equalr.size() >0){
                     /* converting the suffix to an int and saving num_resources */
-                    temp_num_resources = equal.suffix().str();
-                    stringstream num_resources_convert(temp_num_processes);
+                    temp_num_resources = equalr.suffix().str();
+                    stringstream num_resources_convert(temp_num_resources);
                     num_resources_convert >> num_resources;
-                    //cout << "\nthe num_resources is:" <<num_resources<<endl;
+                    cout << "\nthe num_resources is:" <<num_resources<<endl;
                     check_num_units++;
                 }else if(check_num_units == 2 && check_adjacency_matrix != num_resources){ /* ready to receive list of units */
                     /* create string stream from the string */
@@ -91,16 +230,16 @@ void readfile(string textfile,  vector<int> &num_resources_list, vector<vector<i
                             stringstream int_convert_one(substr);
                             int_convert_one >> int_converted_one;
                             num_resources_list.push_back(int_converted_one);
+                            used_num_resources_list.push_back(int_converted_one);
                             check_adjacency_matrix++;
                         }
 
                     }
-                 /*   for(int i = 0; i<result.size(); i++) {    //print all splitted strings
-                       cout <<check_adjacency_matrix<<"*"<< result.at(i) << endl;
-                    } */
+
 
                 }else if(check_adjacency_matrix == num_resources){ /* starting to parse for adjacency matrix */
                     /* create string stream from the string */
+                    
                     stringstream s_stream(line);
                     while(s_stream.good()) {
                         string substr;
@@ -113,37 +252,98 @@ void readfile(string textfile,  vector<int> &num_resources_list, vector<vector<i
                             result.push_back(int_converted_two);
                         }
                     }
-                    a.push_back(result);
-                    result.erase (result.begin(), result.end());
-                 /* for(int i = 0; i<result.size(); i++) {    //print all splitted strings
-                        cout <<","<< result.at(i);
+                   // cout << "entering"<< result.size() <<endl;
+                    if(result.size() >0){
+                        a.push_back(result);
                     }
-                    cout << endl;*/
+
+                    result.erase (result.begin(), result.end());
+
                 }
             }
         }
         configf.close();
         /* creating adjacency list */
-        AdjList = convert(a);
+        
+        AdjList = convert(a,num_processes, num_resources, used_num_resources_list);
         /*close config file*/
     }
 
 
 }
+void printAdjList(vector<vector<int>> AdjList, int num_processes,int num_resources){
+    // print the adjacency list
+    int print=0;
+    for (int i = 0; i < AdjList.size(); i++)
+    {
 
-
-int main(){
+        for(int j = 0; j < AdjList[i].size(); j++)
+        {
+            if(print==0){
+                if(i < num_processes){
+                   // cout << i <<" "<< num_processes<<endl;
+                    cout <<"P"<<i+1;
+                }else if(i >= num_processes){
+                    cout <<"R"<< (i+1 -  num_processes);
+                }
+                print++;
+            }
+            if(j == AdjList[i].size() - 1 )
+            {
+                if( AdjList[i][j] == -1){
+                    cout << " -> " << AdjList[i][j] << endl;
+                    break;
+                }else if(AdjList[i][j] < num_processes){
+                    cout << " -> P" << AdjList[i][j] +1 << endl;
+                    break;
+                }else if(AdjList[i][j] >= num_resources){
+                    cout << " -> R" << (AdjList[i][j] - num_processes) +1 << endl;
+                    break;
+                }
+            }
+            else{
+                if( AdjList[i][j] == -1){
+                    cout << " -> " << AdjList[i][j] ;
+                }else if(AdjList[i][j]<num_processes){
+                    cout << " -> P" << AdjList[i][j] +1;
+                }else if(AdjList[i][j]>=num_resources){
+                     cout << " -> R" << (AdjList[i][j] -  num_processes)+1;
+                }
+            }
+        }
+        print=0;
+    }
+    cout <<endl;
+}
+bool availableResources(vector<vector<int>> &AdjList, int process, int num_processes, int num_resources,vector<int> used_num_resources_list ){
+    bool flag=false;
+    for(int i=0; i< AdjList[process].size(); i++){
+        if(AdjList[process][i] != process){ /* if not the same process check the resource*/
+            if(used_num_resources_list[AdjList[process][i] - num_processes] >0){
+                return true;
+            }
+        }
+    }
+    return false;
+}
+int main(int argc, char *argv[]){
     vector<int> num_resources_list;
+        vector<int> used_num_resources_list;
     vector<vector<int>> AdjList ;
     int num_processes, num_resources;
     string textfile;
     /* text file */
-    cout << "Please enter the configuration file name :";
+    if(argc != 2){
+        cout<<"usage: " << argv[0] << " <filename>\n";
+        exit(1);
+    }else{
+        textfile = argv[1];
+    }
     /*enter the text file */
-    getline(cin, textfile);
+
     /*get the text file*/
     
-    readfile(textfile, num_resources_list, AdjList, num_processes, num_resources);
+    readfile(textfile, num_resources_list, AdjList, num_processes, num_resources, used_num_resources_list);
     cout << "\nthe num_processes is:" << num_processes <<endl;
     cout << "\nthe num_resources is:" << num_resources<<endl;
     
@@ -154,41 +354,63 @@ int main(){
     }
     cout << endl;
     cout<<endl<<"Adjacency List:"<<endl;
-    // print the adjacency list
-    int print=0;
-    for (int i = 0; i < AdjList.size(); i++)
-    {
-
-        for(int j = 0; j < AdjList[i].size(); j++)
+    printAdjList(AdjList, num_processes, num_resources);
+    int times =0;
+    while(times==0){
+        /* checking if a process is a sink */
+        for (int i = 0; i <num_processes; i++)
         {
-            if(print==0){
-                if(i <= num_processes){
-                    cout <<"P"<<i;
-                }else if(i >= num_resources){
-                    cout <<"R"<< (i -  num_processes);
-                }
-                print++;
-            }
-            if(j == AdjList[i].size() - 1)
-            {
-                /* if it is a process */
-                if(AdjList[i][j] <= num_processes){
-                    cout << " -> P" << AdjList[i][j]  << endl;
+
+           bool sink= isSink(AdjList, i,  num_processes,  num_resources );
+           cout << "Is process " << i+1 << " a sink?(R1+R2+R3)=0: " << boolalpha <<(sink==1)<<endl;
+                printAdjList(AdjList, num_processes, num_resources);
+            if(sink == 1){
+                cout << "Reducing it..."<<endl;
+                reduce(AdjList, i,  num_processes,  num_resources, used_num_resources_list);
+                    printAdjList(AdjList, num_processes, num_resources);
+                bool reducable = isFullyReduce(AdjList, i,  num_processes,  num_resources );
+                cout << "Is graph fully reducible(0 processes): " << boolalpha <<(reducable==1)<<endl;
+                    printAdjList(AdjList, num_processes, num_resources);
+                if(reducable == true){
+                    cout << "-------No Deadlock Detected" <<endl;
+                    printAdjList(AdjList, num_processes, num_resources);
+                    times=1;
                     break;
-                }else if(AdjList[i][j] >= num_resources){
-                    cout << " -> R" << (AdjList[i][j] - num_processes)  << endl;
+                }else if(reducable == false){
+                    bool edge_check = request_edge_check(AdjList, i,  num_processes,  num_resources,used_num_resources_list, num_resources_list );
+                     cout << "Can request edges be filled:" << boolalpha <<(edge_check==1)<<endl;
+                    printAdjList(AdjList, num_processes, num_resources);
+                    
+                    if(edge_check==false){
+                        cout << "------Deadlock Detected" <<endl;
+                        printAdjList(AdjList, num_processes, num_resources);
+                        times=1;
+                        break;
+                    }
+                }
+                       
+            }else if(i != num_processes && sink==0){
+                /* check if the resource can be used */
+                bool res_check =availableResources(AdjList, i, num_processes,  num_resources,used_num_resources_list );
+                
+                cout << "Can Process allocate resources:" << boolalpha <<(res_check==1)<<endl;
+                if(res_check == true){
+                    printAdjList(AdjList, num_processes, num_resources);
+                }
+                else if( (res_check ==false) && (i==(num_processes-1)) ){
+                    cout << "****Deadlock Detected" <<endl;
+                    times=1;
                     break;
+                }else if(res_check ==false && i<num_processes){
+                    cout << "checking next process" <<endl;
                 }
+                
             }
-            else{
-                if(AdjList[i][j]<=num_processes){
-                    cout << " -> P" << AdjList[i][j];
-                }else if(AdjList[i][j]>=num_resources){
-                     cout << " -> R" << (AdjList[i][j] -  num_processes);
-                }
-            }
+
         }
-        print=0;
+        //times++;
     }
     cout <<endl;
+
+    
 }
