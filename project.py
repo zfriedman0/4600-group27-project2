c8a4d931e4676f27c90a7157be15dc62fd2dbe6f
SMal95 <sirscroogikins@gmail.com>
2020-04-29 20:03:34 -0500

    Add files via upload

--- project.py
+++ project.py
@@ -1,36 +1,25 @@
 # Sources:
 # https://cmdlinetips.com/2018/01/3-ways-to-read-a-file-and-skip-initial-comments-in-python/
 # https://www.geeksforgeeks.org/python-converting-all-strings-in-list-to-integers/
-# file = open("bar.txt", "r")
 
 def parse_processes():
     temp = 'num_processes='
     with open('test.txt') as f:
         for line in f:
-            # if line.startswith("%"):
-            #     continue
             if temp in line: return int(line[line.find(temp) + len(temp):])
-                #num_processes = int(line[line.find(temp1) + len(temp1):])
-                #print("Number of processes:", num_processes)
-                #continue
 
 def parse_resources():
     temp = 'num_resources='
     with open('test.txt') as f:
         for line in f:
-            # if line.startswith("%"):
-            #     continue
             if temp in line: return int(line[line.find(temp) + len(temp):])
-        #num_resources = int(line[line.find(temp2) + len(temp2):])
-        #print("Number of resources:", num_resources)
-        #continue
 
 def parse_units(num_resources):
     with open('test.txt') as f:
         for line in f:
-            if line.startswith("%"): continue
-            if line.startswith("n"): continue
-            if not line.strip(): continue
+            if line.startswith("%"): continue # skips comments
+            if line.startswith("n"): continue # skips process/resource assignment
+            if not line.strip(): continue # skips blank lines
 
             current_line = line.strip().split(',')
             if len(current_line) == num_resources:
