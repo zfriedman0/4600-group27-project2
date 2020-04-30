e537ada6d6534843b8709685ac66ea30dc78d68b
SMal95 <sirscroogikins@gmail.com>
2020-04-29 19:32:28 -0500

    Add files via upload

--- project.py
+++ project.py
@@ -1,6 +1,7 @@
 # Sources:
 # https://cmdlinetips.com/2018/01/3-ways-to-read-a-file-and-skip-initial-comments-in-python/
 # file = open("bar.txt", "r")
+import re
 
 def parse_processes():
     temp = 'num_processes='
@@ -27,15 +28,25 @@ def parse_resources():
 def parse_units(num_resources):
     with open('test.txt') as f:
         for line in f:
-            if line.startswith("%"): continue
-            if line.startswith("n"): continue
-            if not line.strip(): continue
-            current_line = line.split(",")
-            if len(current_line) == num_resources:
-                for x in current_line:
-                    array[x] = current_line[x]
-                print(current_line)
-                return
+            if line.startswith("%"): continue # skips comments
+            if line.startswith("n"): continue # skips number resource/process definition
+            if not line.strip(): continue # skips blank lines
+
+            #current_line = line.split(",") # delineates commas
+            current_line = re.split(r'[,|\n]', line)
+            print(current_line)
+            # if len(current_line) == num_resources:
+            #     x = 0
+            #     num_units = [None] * num_resources
+            #     while x < (num_resources + 1):
+            #         num_units[x] = current_line[x]
+            #     #print(num_units)
+            #     return
+
+                # for x in current_line:
+                #     array[x] = current_line[x]
+                # print(current_line)
+                # return
             # for x in current_line:
             #     if x > 1
 
